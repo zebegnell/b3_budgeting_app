e8bccf8e7314fdd06ca91cfb6de03569723a4810
= <=>
2022-10-02 22:46:14 -0400

    commit v0.2.

--- Backend/appbackend/appbackend/settings.py
+++ Backend/appbackend/appbackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backoffice.apps.BackofficeConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Custom User
+AUTH_USER_MODEL = 'backoffice.User'
--- Backend/appbackend/backoffice/models.py
+++ Backend/appbackend/backoffice/models.py
@@ -1,11 +1,14 @@
 from asyncio.windows_events import NULL
+from email.policy import default
 from multiprocessing.sharedctypes import Value
+from tabnanny import verbose
 import uuid
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
 from backoffice.models import UserManager
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -13,12 +16,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'),unique=True, db_index=True)
     First_name = models.CharField(_("first name"), max_length=50, blank=True) #what the fuck does blank=True mean
     Last_name = models.CharField(_("last name"), max_length=50, blank=True)
-    googleId = models.CharField() #needed if signing in with google email
-    android = models.BooleanField(blank=True, default=False) #need to update to modern django
-    ios = models.BooleanField(blank=True, default=False, null=True) #need to update to modern django
+    googleToken = models.CharField(max_length=200, null=True, blank=True)
+    connection_choice = ((0,'ios'), (1,'android'),(2,'browser'))
+    connectionType = models.IntegerField(choices=connection_choice, null=True, blank=True)
     is_valid = models.BooleanField(default=True)
     avatar = models.ImageField(upload_to='/avatars', null=True, blank=True)
     lastConnexionDate = models.DateTimeField(null=True, blank=True)
+    user_budget = models.IntegerField()
 
     objects = UserManager()
 
@@ -36,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return u'%s' % (self.email)
 
-class UserManage(BaseUserManager):
+class UserManager(BaseUserManager):
     use_in_migrations = True
     
     def _create_user(self, email, password, **extra_fields):
@@ -62,10 +66,50 @@ class UserManage(BaseUserManager):
             raise ValueError('Superuser must have is_superuser = True.')
         return self._create_user(email, password, **extra_fields)
 
-class Expense(models.Model):
-    def tempshit(self):
-        return "im dumb"
+class Expense(models.Model): #actual expense that user inputs
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    price = models.FloatField(default=0, verbose_name=_('price'))
+    purchaseTime = models.DateTimeField(auto_now_add=True)
 
-class Expense_list(models.Model):
-    def tempshit(self):
-        return "im dumb"
+    def __str__(self):
+        return "%s" % (self.name)
+    
+    def pricenum(self):
+        return self.price
+    
+    def expense_name(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = ('Expense')
+        verbose_name_plural = ('Expenses')
+
+#class Category (models.Model): #Expense categories available to user, can be added to. may not need this. may need to readjust expense and user blocks
+#    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#    name = models.CharField(max_length=100, verbose_name=_('name'))
+#    
+#    class Meta:
+#        verbose_name=_('Category')
+#        verbose_name_plural=_('Categories')
+#
+#    def idString(self):
+#        return str(self.id)
+#
+#    def __str__(self):
+#        return self.name
+
+
+class Expense_list(models.Model): #list of all of user's expenses
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    refUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    totalExpenses = models.FloatField(default=0, verbose_name=_('totalExpenses'))
+
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
+
+    def save(self, *args, **kwargs):
+        super(Expense_list, self).save(*args,**kwargs)
